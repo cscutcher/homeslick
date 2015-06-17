7d20b4ccdc5514702c3c7bad1a9d425908bfca4a
Chris Scutcher <chris.scutcher@ninebysix.co.uk>
2015-06-17 19:22:56 +0100

    Setuptools stuff

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Setup for homeslick
+"""
+from setuptools import setup, find_packages
+setup(
+    name = "homeslick",
+    version = "0.1",
+    packages = find_packages(),
+)
